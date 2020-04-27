90a6da7b98994de96946396e8eed13abac58e2bf
cxt <chuxutao@zhongan.io>
2020-04-27 14:41:33 +0800

    fix bugs
    fix bugs

--- sentry_wechat/plugin.py
+++ sentry_wechat/plugin.py
@@ -85,7 +85,7 @@ class WechatPlugin(notify.NotificationPlugin):
             'help': 'Enter wechat robot url.',
             'placeholder': 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=xxx-xxx-xxx-xxx-xxx',
             'validators': [validate_urls],
-            'required': False
+            'required': True
         }]
 
     def get_webhook_urls(self, project):
@@ -114,7 +114,6 @@ class WechatPlugin(notify.NotificationPlugin):
 
     def notify_users(self, group, event, *args, **kwargs):
         url = self.get_webhook_urls(group.project)
-        link = self.get_group_url(group)
         project = event.project
         host = self.get_option("host", project) or ''
         safe = self.get_option("safe", project)
@@ -124,12 +123,10 @@ class WechatPlugin(notify.NotificationPlugin):
             "text": {
                     "content": '[标题] {}\n[时间] {}\n[{}] {}\n[href]({})'.format(
                         "Sentry {} 项目告警".format(project.slug),
-                        datetime.now(timezone('Asia/Shanghai')
-                                     ).strftime("%Y-%m-%d %H:%M:%S"),
-                        event.get_tag('level').capitalize(
-                        ), event.message.encode('utf8'),
-                        "{}{}events/{}/".format(host,
-                                                group.get_absolute_url(), event.id),link
+                        datetime.now(timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S"),
+                        event.get_tag('level').capitalize(),
+                        event.message.encode('utf8'),
+                        "{}{}events".format(host,group.get_absolute_url())
                     )
             }
         }
--- setup.py
+++ setup.py
@@ -1,32 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+sentry-Dingtalk
+==============
+An extension for Sentry which integrates with Dingtalk. It will send
+notifications to dingtalk robot.
+:copyright: (c) 2017 by guoyong yi, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
 from setuptools import setup, find_packages
+
+# See http://stackoverflow.com/questions/9352656/python-assertionerror-when-running-nose-tests-with-coverage
+# for why we need to do this.
+from multiprocessing import util
+
+
+tests_require = [
+]
+
+install_requires = [
+    'sentry>=9.1.0',
+]
+
 
 setup(
     name='sentry-wechat-plugin-renew',
-    version='1.0.1',
+    version='1.0.5',
     keywords='sentry wechat ',
     author='cxt',
     author_email='cxt1122jk@gmail.com',
     url='https://github.com/susujs/sentry-wechat-plugin.git',
     description='A Sentry extension which integrates with Wechat robot.',
-    long_description='A Sentry extension which integrates with Wechat robot',
-    long_description_content_type="text/markdown",
+    long_description='A Sentry extension which integrates with Wechat robot.',
+    long_description_content_type='text/markdown',
     license='BSD',
-    include_package_data=True,
+    platforms='any',
+    packages=find_packages(exclude=['tests']),
     zip_safe=False,
-    packages=find_packages(),
-    install_requires=[
-        'sentry>=9.0.0',
-        'requests',
-        'pytz',
-        'django',
-    ],
-        entry_points={
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='nose.collector',
+    entry_points={
         'sentry.plugins': [
             'wechat = sentry_wechat.plugin:WechatPlugin'
         ],
     },
+    include_package_data=True,
     classifiers=[
         'Framework :: Django',
         'Intended Audience :: Developers',
